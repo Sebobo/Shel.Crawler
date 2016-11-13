0dc788de347d10aacab0fbe2a6499d391d4b6e31
Sebastian Helzle <sebastian@helzle.it>
2016-11-13 17:22:29 +0100

    FEATURE: Implemented curl based crawl command

--- Classes/Command/CrawlerCommandController.php
+++ Classes/Command/CrawlerCommandController.php
@@ -6,12 +6,17 @@ namespace Shel\Crawler\Command;
  * This script belongs to the Flow plugin Shel.Crawler                    *
  *                                                                        */
 
+use RollingCurl\Request;
+use Shel\Crawler\Service\SitemapService;
 use TYPO3\Flow\Annotations as Flow;
 use TYPO3\Flow\Cli\CommandController;
+use TYPO3\Flow\Utility\Now;
 use TYPO3\Neos\Controller\Frontend\NodeController;
+use TYPO3\Neos\Domain\Service\ContentContextFactory;
 use TYPO3\TYPO3CR\Domain\Model\NodeInterface;
 use TYPO3\TYPO3CR\Domain\Model\Workspace;
 use TYPO3\TYPO3CR\Domain\Repository\WorkspaceRepository;
+use TYPO3\TYPO3CR\Domain\Service\Context;
 
 /**
  *
@@ -21,21 +26,24 @@ class CrawlerCommandController extends CommandController
 {
     /**
      * @Flow\Inject
-     *
      * @var \TYPO3\Flow\Log\SystemLoggerInterface
      */
     protected $systemLogger;
 
     /**
      * @Flow\Inject
-     *
+     * @var ContentContextFactory
+     */
+    protected $contentContextFactory;
+
+    /**
+     * @Flow\Inject
      * @var \TYPO3\TYPO3CR\Domain\Repository\NodeDataRepository
      */
     protected $nodeDataRepository;
 
     /**
      * @Flow\Inject
-     *
      * @var WorkspaceRepository
      */
     protected $workspaceRepository;
@@ -47,13 +55,25 @@ class CrawlerCommandController extends CommandController
     {
         /** @var Workspace $liveWorkspace */
         $liveWorkspace = $this->workspaceRepository->findByName('live')->getFirst();
-
         $rootNode = $liveWorkspace->getRootNodeData();
 
-        $nodes = $this->nodeDataRepository->findByParentAndNodeTypeRecursively(
+        /** @var Context $context */
+        $context = $this->contentContextFactory->create([
+            'workspaceName' => 'live',
+            'currentDateTime' => new Now(),
+            'dimensions' => [],
+            'targetDimensions' => [],
+            'invisibleContentShown' => false,
+            'removedContentShown' => false,
+            'inaccessibleContentShown' => false
+        ]);
+
+        /** @var array<\TYPO3\TYPO3CR\Domain\Model\NodeInterface> $nodes */
+        $nodes = $this->nodeDataRepository->findByParentAndNodeTypeInContext(
             $rootNode->getPath(),
             'TYPO3.Neos:Document',
-            $liveWorkspace
+            $context,
+            true
         );
 
         $nodeController = $this->objectManager->get(NodeController::class);
@@ -65,5 +85,37 @@ class CrawlerCommandController extends CommandController
             print_r($result);
             return;
         }
+    }
+
+    /**
+     * @param string $url of sitemap which should be crawled
+     * @return bool
+     */
+    public function crawlSitemapCommand($url)
+    {
+        $sitemapService = $this->objectManager->get(SitemapService::class);
+
+        $start = microtime(true);
+        $this->outputLine('Fetching sitemap...');
+        $urls = $sitemapService->retrieveSitemap($url);
+
+        if ($urls === false) {
+            $this->outputFormatted('Failed fetching sitemap at %s', [$url]);
+            return false;
+        }
+        $this->outputFormatted('...done in %f', [microtime(true) - $start]);
+
+        // Start crawling the urls from the sitemap
+        $urlCount = count($urls);
+        $start = microtime(true);
+        $this->outputFormatted('Fetching %d urls...', [count($urls)]);
+        $sitemapService->crawlUrls($urls, function ($completed, Request $request) use ($urlCount) {
+            preg_match_all("#.*<title>(.*)</title>.*#iU", $request->getResponseText(), $matches);
+            $pageTitle = isset($matches[1][0]) ? $matches[1][0] : 'No page title';
+            $this->outputFormatted('(%d/%d) Fetch complete for (%s) - %s', [$completed, $urlCount, $request->getUrl(), $pageTitle]);
+        });
+        $this->outputFormatted('...done in %f', [microtime(true) - $start]);
+
+        return true;
     }
 }
---
+++ Classes/Service/SitemapService.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace Shel\Crawler\Service;
+
+/*                                                                        *
+ * This script belongs to the Flow plugin Shel.Crawler                    *
+ *                                                                        */
+
+use RollingCurl\Request;
+use RollingCurl\RollingCurl;
+use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Flow\Http\Client\Browser;
+use TYPO3\Flow\Http\Client\CurlEngine;
+
+/**
+ * @Flow\Scope("singleton")
+ */
+class SitemapService
+{
+    /**
+     * @var array options for sitemap curl requests
+     */
+    protected $requestEngineOptions = [
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_CONNECTTIMEOUT => 30,
+        CURLOPT_FOLLOWLOCATION => 1,
+        CURLOPT_SSL_VERIFYHOST => 0,
+        CURLOPT_SSL_VERIFYPEER => 0,
+    ];
+
+    /**
+     * @var array options for crawler curl requests
+     */
+    protected $crawlRequestOptions = [
+        CURLOPT_AUTOREFERER => 1,
+        CURLOPT_FORBID_REUSE => 0,
+        CURLOPT_FRESH_CONNECT => 0,
+        CURLOPT_USERAGENT => 'ShelCrawlerBot',
+        CURLOPT_NOBODY => 1,
+        CURLOPT_HEADER => 0,
+        CURLOPT_SSL_VERIFYHOST => 0,
+        CURLOPT_SSL_VERIFYPEER => 0,
+    ];
+
+    /**
+     * Add a sitemap URL to our crawl stack. Sitemap index files works too.
+     *
+     * @param string $url URL to a XML sitemap
+     * @param array $options Additional options for the curl engine
+     * @return array|bool False if sitemap cannot be fetched or array with locs
+     */
+    public function retrieveSitemap($url, $options = [])
+    {
+        $browser = $this->getBrowser($options);
+        $response = $browser->request($url);
+
+        if ('200' != $response->getStatusCode()) {
+            return false;
+        }
+
+        $xml = new \SimpleXMLElement($response->getContent(), LIBXML_NOBLANKS);
+
+        if (!$xml) {
+            return false;
+        }
+
+        $locs = [];
+
+        switch ($xml->getName()) {
+            case 'sitemapindex':
+                // Retrieve sitemap for each item in index
+                foreach ($xml->sitemap as $sitemap) {
+                    $result = $this->retrieveSitemap(reset($sitemap->loc));
+                    if ($result === false) {
+                        return false;
+                    }
+                    $locs = array_merge($locs, $result);
+                }
+                break;
+            case 'urlset':
+                foreach ($xml->url as $entry) {
+                    $loc = reset($entry->loc) . '';
+                    $locs[$loc] = true;
+                }
+                break;
+        }
+
+        return array_keys($locs);
+    }
+
+    /**
+     * @param array $urls
+     * @param callable $callback will be called for each completed request
+     * @param array $options additional curl options to be set
+     * @param int $simultaneousLimit number of parallel curl requests
+     * @return bool
+     */
+    public function crawlUrls($urls, $callback, $options = [], $simultaneousLimit = 10)
+    {
+        $rollingCurl = new RollingCurl();
+        foreach ($urls as $url) {
+            $rollingCurl->get($url);
+        }
+        $rollingCurl
+            ->addOptions(array_merge($this->crawlRequestOptions, $options))
+            ->setCallback(function(Request $request, RollingCurl $rollingCurl) use ($callback) {
+                if ($rollingCurl->countPending() % 100 == 0) {
+                    $rollingCurl->clearCompleted();
+                }
+                $callback($rollingCurl->countCompleted(), $request);
+            })
+            ->setSimultaneousLimit($simultaneousLimit)
+            ->execute();
+        ;
+        return true;
+    }
+
+    /**
+     * @param array $options Additional options for the curl engine
+     * @return Browser
+     */
+    protected function getBrowser($options = [])
+    {
+        $browser = new Browser();
+        $curlEngine = new CurlEngine();
+        $options = array_merge($this->requestEngineOptions, $options);
+        foreach ($options as $option => $value) {
+            $curlEngine->setOption($option, $value);
+        }
+        $browser->setRequestEngine($curlEngine);
+        return $browser;
+    }
+}
